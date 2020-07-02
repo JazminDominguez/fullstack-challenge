c67b29f9c93030a8743e474ab9c89dccd35eac4d
Jaz Dominguez <j.dgez.p@gmail.com>
2020-07-02 12:32:08 -0500

    Add fetch endpoint function and shows simple map list

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
 import DirectoryList from "ui/components/DirectoryList";
 
-const fetchOffersList = () => {
-    return [];
+const URL =
+    "https://e6di35qzm7.execute-api.us-west-2.amazonaws.com/latest/directory";
+
+/*const fetchOffersList = () => {
+  fetch(URL)
+  .then(response => response.json())
+  .then( res => {
+      return res.data
+  })
+  .catch(err => {
+      console.log(err)
+  });
+  
 };
+*/
 
 export default function App() {
+    const [offerList, setOfferList] = useState([]);
+
+    useEffect(() => {
+        fetch(URL)
+            .then(response => response.json())
+            .then(res => {
+                setOfferList(res.data);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }, []);
+
     return (
         <>
             <p class="py-4 text-lg text-center">Start here :) Good luck!</p>
 
-            <DirectoryList offerList={fetchOffersList()} />
+            <DirectoryList offerList={offerList} />
         </>
     );
 }
--- src/ui/components/DirectoryList.js
+++ src/ui/components/DirectoryList.js
@@ -1,5 +1,12 @@
 export default function DirectoryList({ offerList, ...props }) {
     log.debug("Here is your offers list", offerList);
 
-    return <></>;
+    const mapOfferList = offerList.map(offer => {
+        return (
+            <div key={offer.id}>
+                <h3>{offer.name}</h3>
+            </div>
+        );
+    });
+    return <div>{mapOfferList}</div>;
 }
