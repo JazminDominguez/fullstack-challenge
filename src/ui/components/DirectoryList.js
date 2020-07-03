d372b9ed625bbe21138e4e792aaa84c6d5b977f1
Jaz Dominguez <j.dgez.p@gmail.com>
2020-07-02 19:35:17 -0500

    Convert timestamp into date formant. Modify date font-size

--- src/ui/components/DirectoryList.js
+++ src/ui/components/DirectoryList.js
@@ -4,11 +4,15 @@ export default function DirectoryList({ offerList, ...props }) {
     log.debug("Here is your offers list", offerList);
 
     const mapOfferList = offerList.map(offer => {
+        // converts timestamp to date using toLocalDateString (spanish - Mexico format)
+        let unixTime = offer.created;
+        let date = new Date(unixTime * 1000);
+
         return (
             <Box
                 key={offer.id}
                 width={[1, 1, 1, 1 / 4]}
-                px={[4, 3]}
+                px={[4, 3, 4]}
                 py={[3, 3, 4]}
                 my={[2, 3]}
                 mx={[0, 1, 3]}
@@ -16,7 +20,7 @@ export default function DirectoryList({ offerList, ...props }) {
                 <h3>{offer.name}</h3>
                 <Flex flexDirection="row">
                     <h4>Created:</h4>
-                    <p>{offer.created}</p>
+                    <p>{date.toLocaleDateString("es-MX")}</p>
                 </Flex>
             </Box>
         );
