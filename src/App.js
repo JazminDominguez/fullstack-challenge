918d044742417f6cc937f145db160d56668af020
Jaz Dominguez <j.dgez.p@gmail.com>
2020-07-03 10:59:03 -0500

    Add pagination function and styles

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./index.scss";
 
 export default function App() {
     const [offerList, setOfferList] = useState([]);
-    //default value 1 to fetch the first page
+    // default value 1 to fetch the first page
     const [page, setPage] = useState(1);
 
     const URL = `https://e6di35qzm7.execute-api.us-west-2.amazonaws.com/latest/directory?epp=10&p=${page}`;
@@ -21,13 +21,14 @@ export default function App() {
             });
     }, [page]);
 
-    //known numbers of pages from the API whith 10 elements per page.
+    // known numbers of pages from the API whith 10 elements per page.
     const pagesArray = [1, 2, 3];
 
-    //takes the pagesArray and maps through it to generate the pagination to avoid code repeat for the buttons
+    // takes the pagesArray and maps through it to generate the pagination to avoid code repeat for the buttons
     const pagination = pagesArray.map(index => {
         return (
             <button
+                key={index}
                 onClick={() => setPage(index)}
                 className={page === index ? "-active" : "-inactive"}>
                 {index}
